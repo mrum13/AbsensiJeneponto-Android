ff15d9a023c850ac2ead06074b8a48e568d1a914
mrum13 <muhammadrum135@.com>
2022-08-02 23:06:00 +0800

    home screen not yet

--- app/src/main/java/com/example/absensijeneponto/LoginActivity.java
+++ app/src/main/java/com/example/absensijeneponto/LoginActivity.java
@@ -2,13 +2,29 @@ package com.example.absensijeneponto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button btnLogin;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnLogin = findViewById(R.id.btn_login);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/absensijeneponto/MainActivity.java
+++ app/src/main/java/com/example/absensijeneponto/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.absensijeneponto;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavbar);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+        loadFragment(new HomeFragment());
+    }
+
+    private boolean loadFragment(Fragment fragment){
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.frame_layout, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+        switch (item.getItemId()){
+            case R.id.mHome:
+                fragment = new HomeFragment();
+                break;
+            case R.id.mAttendance:
+                fragment = new AttendanceFragment();
+                break;
+            case R.id.mAccount:
+                fragment = new AccountFragment();
+                break;
+        }
+        return loadFragment(fragment);
     }
 }
